5b715991fe8e8cb86afd9833bf7cfbfac7b56169
David Terral <david.terral@utexas.edu>
2019-10-16 10:07:53 -0500

    generate data now working for uniform and normal dist of integers

--- data_generator/frontend/src/reducers/form.js
+++ data_generator/frontend/src/reducers/form.js
@@ -12,14 +12,32 @@ const initialState = {
 }
 
 
-function generateIntegers(dist, opts) {
+function gaussian(mean, stdev) {
+    var y2;
+    var use_last = false;
+    return function () {
+        var y1;
+        if (use_last) {
+            y1 = y2;
+            use_last = false;
+        }
+        else {
+            var x1, x2, w;
+            do {
+                x1 = 2.0 * Math.random() - 1.0;
+                x2 = 2.0 * Math.random() - 1.0;
+                w = x1 * x1 + x2 * x2;
+            } while (w >= 1.0);
+            w = Math.sqrt((-2.0 * Math.log(w)) / w);
+            y1 = x1 * w;
+            y2 = x2 * w;
+            use_last = true;
+        }
 
-    if (dist == "uniform") {
-        var min = opts.min;
-        var max = opts.max;
-    } else if (dist == "normal") {
-        var mean = opts.mean;
-        var standard_deviation = opts.standard_deviation;
+        var retval = mean + stdev * y1;
+        if (retval > 0)
+            return retval;
+        return -retval;
     }
 }
 
@@ -98,12 +116,18 @@ export default function (state = initialState, action) {
                 for (var j = 0; j < cols[i]; j++) {
                     if (types[i] == "integer") {
                         if (colOpts[i].dist == "uniform") {
-                            var max = 100;
+                            var min = Math.ceil(colOpts[i].opts.min);
+                            var max = Math.floor(colOpts[i].opts.max);
                             for (var k = 0; k < rows; k++) {
-                                arr[k][currentCol] = Math.floor(Math.random() * max);
+                                arr[k][currentCol] = Math.floor(Math.random() * (max - min + 1)) + min;
                             }
                         } else if (colOpts[i].dist == "normal") {
-
+                            var mean = colOpts[i].opts.mean;
+                            var stdev = colOpts[i].opts.standard_deviation;
+                            var normal_dist = gaussian(mean, stdev);
+                            for (var k = 0; k < rows; k++) {
+                                arr[k][currentCol] = Math.floor(normal_dist());
+                            }
                         }
 
                     }
